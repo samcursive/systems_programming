831f5be20ced83b9b79a0219c7add58e126fc016
Sam Arshad <jsb211@txstate.edu>
2025-04-22 10:11:55 -0500

    Added changes to code, created global arrays, macros and cleaned up code I copied from my Linux Server.

--- project.c
+++ project.c
@@ -21,6 +21,13 @@ Due Date : April 23 2025
 #define TIME_QUANTUM 100
 
 
+// Global Arrays
+
+int arrival_times[NUM_PROCESSES];
+int durations[NUM_PROCESSES];
+int priorities[NUM_PROCESSES];
+int process_id[NUM_PROCESSES];
+
 
 
 int main(void){
@@ -48,67 +55,8 @@ int main(void){
     printf("\nNum times the loop ran:%d\n ", count);
 
 
-	printf("\n\n\n\n\n\n\nDebug Line\n\n\n");
-	return 0;
-}
-
-
-
-
-
-
-
-#define NUM_PROCESSES 10
-#define TIME_QUANTUM 100
-
-
-
-
-// Global Arrays
-
-int arrival_times[NUM_PROCESSES];
-int durations[NUM_PROCESSES];
-int priorities[NUM_PROCESSES];
-int process_id[NUM_PROCESSES];
-
-
-int main(){
-
-	//Header
-	printf("\nNAME : Sam Arshad\n");
-	printf("Course Nmber / Name : CS 4350 - UNIX Systems Programming\n");
-	printf("Assignment Number : Project Assignment\n");
-	printf("Due Date : April 23rd 2025\n");
-	printf("\n\n\n\n");
-
-
-
-
-
-	//intro
-	printf("\nWelcome to Sam Arshad's Thread Program\n");
-	printf("This program will simulate the CPU scheduling Algorithms in the following order:\n\n");
-	printf("\n\t- First Come, First Served");
-	printf("\n\t- Shortest Job First\n\t- Priority Queue\n\t- Round Robin\n");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	printf("\nProgrammed by Sam Arshad\n 4-23-2025\n");
+    printf("\nProgrammed by Sam Arshad\n 4-23-2025\n");
 	printf("\n\nDebug Line\n\n\n");
+	printf("\n\n\n\n\n\n\nDebug Line\n\n\n");
 	return 0;
 }
