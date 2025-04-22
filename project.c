fd4824ab35d4810cd6bc121cfe978497fc680ef1
Sam Arshad <jsb211@txstate.edu>
2025-04-22 10:09:24 -0500

    Added in a new library and added commits

--- project.c
+++ project.c
@@ -12,6 +12,64 @@ Due Date : April 23 2025
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+
+
+
+#define NUM_PROCESSES 10
+#define TIME_QUANTUM 100
+
+
+
+
+int main(void){
+
+	//Header
+	printf("\nNAME : Sam Arshad\n");
+	printf("Course Nmber / Name : CS 4350 - UNIX Systems Programming\n");
+	printf("Assignment Number : Project Assignment\n");
+	printf("Due Date : April 23rd 2025\n");
+	printf("\n\n\n");
+
+
+	//intro
+	printf("\nWelcome to Sam Arshad's Thread Program\n");
+	printf("This program will simulate the CPU scheduling Algorithms in the following order:\n\n");
+	printf("\n\t- First Come, First Served");
+	printf("\n\t- Shortest Job First\n\t- Priority Queue\n\t- Round Robin\n");
+
+
+    int farenheit = 0, count = 0;
+    while (farenheit <= 300){
+        farenheit += 10;
+        count++;
+    }
+    printf("\nNum times the loop ran:%d\n ", count);
+
+
+	printf("\n\n\n\n\n\n\nDebug Line\n\n\n");
+	return 0;
+}
+
+
+
+
+
+
+
+#define NUM_PROCESSES 10
+#define TIME_QUANTUM 100
+
+
+
+
+// Global Arrays
+
+int arrival_times[NUM_PROCESSES];
+int durations[NUM_PROCESSES];
+int priorities[NUM_PROCESSES];
+int process_id[NUM_PROCESSES];
 
 
 int main(){
@@ -21,7 +79,10 @@ int main(){
 	printf("Course Nmber / Name : CS 4350 - UNIX Systems Programming\n");
 	printf("Assignment Number : Project Assignment\n");
 	printf("Due Date : April 23rd 2025\n");
-	printf("\n\n\n");
+	printf("\n\n\n\n");
+
+
+
 
 
 	//intro
@@ -47,7 +108,7 @@ int main(){
 
 
 
-
+	printf("\nProgrammed by Sam Arshad\n 4-23-2025\n");
 	printf("\n\nDebug Line\n\n\n");
 	return 0;
 }
