c96f219f2140befedd891e4377344e238faebbbe
Sam Arshad <jsb211@txstate.edu>
2025-04-21 22:34:41 -0500

    Updated Git Ignore, added some code.

--- project.c
+++ project.c
@@ -1,15 +1,53 @@
+/*
+
+NAME : Sam Arshad
+Course Number / Name : CS 4350-251: UNIX System Programming
+Assignment Numbeer: Last Assignment - Project 
+Due Date : April 23 2025
+*/
+
+
+
+
+
 #include <stdlib.h>
 #include <stdio.h>
 
 
 int main(){
 
-    printf("Hello World!");
+	//Header
+	printf("\nNAME : Sam Arshad\n");
+	printf("Course Nmber / Name : CS 4350 - UNIX Systems Programming\n");
+	printf("Assignment Number : Project Assignment\n");
+	printf("Due Date : April 23rd 2025\n");
+	printf("\n\n\n");
+
+
+	//intro
+	printf("\nWelcome to Sam Arshad's Thread Program\n");
+	printf("This program will simulate the CPU scheduling Algorithms in the following order:\n\n");
+	printf("\n\t- First Come, First Served");
+	printf("\n\t- Shortest Job First\n\t- Priority Queue\n\t- Round Robin\n");
 
 
 
 
 
 
-    return EXIT_SUCCESS;
+
+
+
+
+
+
+
+
+
+
+
+
+
+	printf("\n\nDebug Line\n\n\n");
+	return 0;
 }
